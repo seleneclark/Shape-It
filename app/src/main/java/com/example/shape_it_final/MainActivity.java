4638737e6d5e6e44122d34bd3e7d873fd5bab263
Darcee Quebe <darceerl@byui.edu>
2019-11-05 20:16:25 -0700

    Connected button to startGame() function.

--- app/src/main/java/com/example/shape_it_final/MainActivity.java
+++ app/src/main/java/com/example/shape_it_final/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    void startGame(View view) {
+    public void startGame(View view) {
 
     }
 
