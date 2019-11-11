28c26199743c01767649e38116fcad8c0113e18d
Kimi Campbell <kimikandy@gmail.com>
2019-11-10 23:48:51 -0700

    added shape class

---
+++ app/src/main/java/com/example/shape_it_final/Shape.java
@@ -0,0 +1,23 @@
+package com.example.shape_it_final;
+
+public class Shape implements GameItem {
+
+    Shape(){
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void showsName() {
+
+    }
+
+    @Override
+    public void saysName() {
+
+    }
+}
