8b77e4b5956a6c85355c1a3194eef127d9c3a1c8
Darcee Quebe <darceerl@byui.edu>
2019-11-27 20:36:58 -0700

    Fixed random num generator to not repeat shapes.

--- app/src/main/java/com/example/shape_it_final/ShapeFactory.java
+++ app/src/main/java/com/example/shape_it_final/ShapeFactory.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 public class ShapeFactory {
 
+    int previousNumber;
+
     /*//this is called by GameActivity to send back the correct shape depending on the ID
     public GameItem getShape(ImageButton shapeButton, TextView shapeName, int currentShapeID) {
         if (currentShapeID == 0) { //<-- 0 returns triangle shape
@@ -32,26 +34,38 @@ public class ShapeFactory {
      */
 
     public GameItem getShape(ImageButton shapeButton, TextView shapeName) {
-        int numberGameItems = 8; //right now its Circle, Triangle, Square
+        int numberGameItems = 8;
         Random random = new Random();
         int randomNumber = random.nextInt(numberGameItems);
 
+        while (randomNumber == previousNumber) {
+            randomNumber = random.nextInt(numberGameItems);
+        }
+
         switch (randomNumber) {
             case 0:
+                previousNumber = 0;
                 return new Triangle(shapeButton, shapeName);
             case 1:
+                previousNumber = 1;
                 return new Circle(shapeButton, shapeName);
             case 2:
+                previousNumber = 2;
                 return new Square(shapeButton, shapeName);
             case 3:
+                previousNumber = 3;
                 return new Rectangle(shapeButton, shapeName);
             case 4:
+                previousNumber = 4;
                 return new Star(shapeButton, shapeName);
             case 5:
+                previousNumber = 5;
                 return new Oval(shapeButton, shapeName);
             case 6:
+                previousNumber = 6;
                 return new Heart(shapeButton, shapeName);
             case 7:
+                previousNumber = 7;
                 return new Pentagon(shapeButton, shapeName);
                 default:
                     return null;
