c13361acccc48a7c00842bb466cf4904bd00a9ed
Selenis <50272834+seleneclark@users.noreply.github.com>
2019-12-05 15:09:30 -0700

    Created simple tests for ShapeActivity and ColorActivity

--- app/src/androidTest/java/com/example/shape_it_final/MainActivityTest.java
+++ app/src/androidTest/java/com/example/shape_it_final/MainActivityTest.java
@@ -10,10 +10,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-import static androidx.core.content.ContextCompat.startActivity;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -34,38 +40,61 @@ import static org.junit.Assert.*;
 public class MainActivityTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> _ActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-    private MainActivity _Activity = null;
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private MainActivity activity = null;
 
     @Before
     public void setUp() {
         try {
-            _Activity = _ActivityTestRule.getActivity();
+            activity = activityTestRule.getActivity();
         } catch (Exception e) {
             Log.e("SHAPEIT", "Exception thrown in set up test");
         }
     }
 
     @Test
+    public void test_isActivityInView() {
+        onView(withId(R.id.main)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void test_visibility_ShapesButton() {
+        onView(withId(R.id.ShapesButton)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void test_visibility_ColorsButton() {
+        //an alternate way to test "matches(isDisplayed)"
+        onView(withId(R.id.ColorsButton)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+    @Test
+    public void test_isTitleTextDisplayed() {
+        onView(withId(R.id.welcomeText)).check(matches(withText(R.string.welcomeMessage)));
+
+    }
+
+
+
+
+    @Test
     public void testLaunch() {
-        View view = _Activity.findViewById(R.id.welcomeText);
+        View view = activity.findViewById(R.id.welcomeText);
         assertNotNull(view);
     }
 
     @Test
     public void testStartButton() {
-        assertNotNull(_Activity.findViewById(R.id.startButton));
+        assertNotNull(activity.findViewById(R.id.ShapesButton));
     }
+
     @Test
     public void testLaunchOfGameActivity() {
-        Intent startGameActivity = _Activity.getIntent();
+        Intent startGameActivity = activity.getIntent();
         assertNotNull("Intent is null", startGameActivity);
     }
 
     @After
     public void tearDown() {
         try {
-        _Activity = null;
+        activity = null;
         } catch (Exception e) {
             Log.e("SHAPEIT", "Exception thrown in teardown test");
         }
---
+++ app/src/androidTest/java/com/example/shape_it_final/ShapeActivityTest.java
@@ -0,0 +1,53 @@
+package com.example.shape_it_final;
+
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class ShapeActivityTest {
+
+    @Rule
+    public ActivityTestRule<ShapeActivity> shapeActivityTestRule = new ActivityTestRule<>(ShapeActivity.class);
+    private ShapeActivity shapeActivityTest = null;
+
+    @Before
+    public void setUp() throws Exception {
+
+        try {
+            shapeActivityTest = shapeActivityTestRule.getActivity();
+        } catch (Exception e) {
+            Log.e("SHAPEIT", "Exception thrown in set up test");
+        }
+    }
+    @Test
+    public void test_isShapeActivityInView() {
+        onView(withId(R.id.shape)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void test_visibility_ShapeItemOnTheScreen() {
+        onView(withId(R.id.imageButton)).check(matches(isDisplayed()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        try {
+            shapeActivityTest = null;
+        } catch (Exception e) {
+            Log.e("SHAPEIT", "Exception thrown in teardown test of ShapeActivity");
+        }
+    }
+
+}
